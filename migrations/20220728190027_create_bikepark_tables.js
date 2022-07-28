706b7504ed765d67837080e313bfa159ccc28fad
Valerie Pomerleau <v.pomerleau@gmail.com>
2022-07-28 13:00:41 -0700

    begin connecting auth0

--- migrations/20220728190027_create_bikepark_tables.js
+++ migrations/20220728190027_create_bikepark_tables.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
     return knex.schema
       .createTable('rider', (table) => { // fill with table columns
-        // table.increments('id').primary();
+        table.increments('id').primary();
         // table.string('name').notNullable();
         // table.string('position').notNullable().defaultTo('Store Manager');
         // table.string('manager').notNullable();
@@ -10,26 +10,10 @@ exports.up = function (knex) {
         // table.string('email').notNullable();
         // table.timestamp('updated_at').defaultTo(knex.fn.now());
       })
-      .createTable('booking', (table) => { // fill with table columns
-        // table.increments('id').primary();
-        // table.string('name').notNullable();
-        // table.string('description').notNullable();
-        // table
-        //   .integer('warehouse_id')
-        //   .unsigned()
-        //   .notNullable()
-        //   .references('id')
-        //   .inTable('warehouse')
-        //   .onUpdate('CASCADE')
-        //   .onDelete('CASCADE');
-        // table.integer('quantity').notNullable().defaultTo(0);
-        // table.string('status').notNullable();
-        // table.timestamp('updated_at').defaultTo(knex.fn.now());
-      });
-      // repeat for other tables (e.g., product, etc.)
+      // repeat for other tables (e.g., bookings, product, etc.)
   };
   
   exports.down = function (knex) {
-    return knex.schema.dropTable('booking').dropTable('rider'); // chain databases according to foreign keys
+    return knex.schema.dropTable('rider'); // chain databases according to foreign keys (tables with fkey first)
   };
   
