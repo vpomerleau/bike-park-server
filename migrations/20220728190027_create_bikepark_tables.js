91f599fb4530ac9f919d0470c814e972ce8674bc
Valerie Pomerleau <v.pomerleau@gmail.com>
2022-08-05 14:15:51 -0700

    update migrations file to create tables for purchase data

--- migrations/20220728190027_create_bikepark_tables.js
+++ migrations/20220728190027_create_bikepark_tables.js
@@ -19,17 +19,31 @@ exports.up = function (knex) {
       })
       .createTable('rider_product', (table)=>{
         table.increments('id').unsigned().primary();
-        table.integer('quantity').unsigned().notNullable();
         table.integer('rider_id').unsigned();
         table.integer('product_id').unsigned();
+        table.integer('quantity').unsigned().notNullable();
         table.foreign('rider_id').references('id').inTable('riders').onUpdate("CASCADE").onDelete("CASCADE");
         table.foreign('product_id').references('id').inTable('products').onUpdate("CASCADE").onDelete("CASCADE");
       })
-      // repeat for other tables (transactions, product_transaction, rider_transaction, account, rider_account, waivers, checkins...)
+      .createTable('transactions', (table)=>{
+        table.increments('id').unsigned().primary();
+        table.string('stripe_payment_id').unique().notNullable();
+        table.string('transaction_status').notNullable();
+        table.integer('rider_id').unsigned().notNullable();
+        table.foreign('rider_id').references('id').inTable('riders').onUpdate("CASCADE").onDelete("CASCADE");
+      })
+      .createTable('product_transaction', (table)=>{
+        table.increments('id').unsigned().primary();
+        table.integer('transaction_id').unsigned().notNullable();
+        table.integer('product_id').unsigned().notNullable();
+        table.foreign('transaction_id').references('id').inTable('transactions').onUpdate("CASCADE").onDelete("CASCADE");
+        table.foreign('product_id').references('id').inTable('products').onUpdate("CASCADE").onDelete("CASCADE");
+      })
+      // repeat for other tables (product_transaction, rider_transaction, account, rider_account, waivers, checkins...)
   };
   
   exports.down = function (knex) {
     // chain tables according to foreign keys (tables with fkey first)
-    return knex.schema.dropTable('rider_product').dropTable('products').dropTable('riders'); 
+    return knex.schema.dropTable('product_transaction').dropTable('transactions').dropTable('rider_product').dropTable('products').dropTable('riders'); 
   };
   
