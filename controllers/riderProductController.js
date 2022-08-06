62957e0ee328972beec2df69e447db851bb404a0
Valerie Pomerleau <v.pomerleau@gmail.com>
2022-08-06 16:35:11 -0700

    add route to post new rider products

--- controllers/riderProductController.js
+++ controllers/riderProductController.js
@@ -8,24 +8,35 @@ exports.index = (req, res) => {
     .then((data) => {
       res.status(200).json(data);
     })
-    .catch((err) => res.status(400).send(`Error retrieving list of all rider products: ${err}`));
+    .catch((err) =>
+      res
+        .status(400)
+        .send(`Error retrieving list of all rider products: ${err}`)
+    );
 };
 
 exports.productsForOneRider = (_req, res) => {
   const { riderId } = _req.params;
 
   knex("rider_product")
-    .where({ rider_id:riderId })
+    .where({ rider_id: riderId })
     .then((data) => {
       res.status(200).json(data);
     })
-    .catch((err) => res.status(400).send(`Error retrieving rider products: ${err}`));
+    .catch((err) =>
+      res.status(400).send(`Error retrieving rider products: ${err}`)
+    );
 };
 
 exports.new = (req, res) => {
   knex("rider_product")
     .insert(req.body)
     .then(() => {
-      res.status(201).send("Rider product added");
+      knex("rider_product")
+        .select("product_id", "quantity")
+        .where("rider_id", req.body.rider_id)
+        .then((data) => {
+          res.status(201).send(data);
+        });
     });
 };
