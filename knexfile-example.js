6611e5f3a1775d0ea17ac4576c01779e6f408ad7
Valerie Pomerleau <v.pomerleau@gmail.com>
2022-08-13 17:26:20 -0700

    update knexfile for production

--- knexfile-example.js
+++ knexfile-example.js
@@ -3,15 +3,24 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-module.exports = {
+const connections = {
   development: {
     client: "mysql",
     connection: {
       host: "127.0.0.1",
       user: "root",
-      password: '', //YOUR MYSQL PASSWORD GOES HERE
+      password: "", //YOUR MYSQL PASSWORD GOES HERE
       database: "bikepark",
       charset: "utf8",
     },
   },
+  production: {
+    client: "mysql",
+    connection: process.env.JAWSDB_URL,
+  },
 };
+
+module.exports =
+  process.env.NODE_ENV === "production"
+    ? connections.production
+    : connections.development;
