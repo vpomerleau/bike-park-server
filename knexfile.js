9d130d7eeb1ad7da4f01d1a2a1b301d9413e21aa
Valerie Pomerleau <v.pomerleau@gmail.com>
2022-08-13 17:36:44 -0700

    take 2 adding knexfile

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+const connections = {
+  development: {
+    client: "mysql",
+    connection: {
+      host: "127.0.0.1",
+      user: "root",
+      password: "", //YOUR MYSQL PASSWORD GOES HERE
+      database: "bikepark",
+      charset: "utf8",
+    },
+  },
+  production: {
+    client: "mysql",
+    connection: process.env.JAWSDB_URL,
+  },
+};
+
+module.exports =
+  process.env.NODE_ENV === "production"
+    ? connections.production
+    : connections.development;
